3293a7aadf53caa4d909eeec862ae3a30ff2d472
Guilherme Ribeiro <augusto.gui.2208@gmail.com>
2023-01-07 18:28:43 -0300

    StoryBook update

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,26 @@
+import { AvatarProps, Avatar } from '@ignite-ui/react'
+import { Meta, StoryObj } from '@storybook/react'
+
+export default {
+  title: 'Data display/Avatar',
+  component: Avatar,
+  args: {
+    src: 'https://github.com/guilherme-santos08.png',
+    alt: 'Guilherme Ribeiro',
+  },
+
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
+} as Meta<AvatarProps>
+
+export const Primary: StoryObj<AvatarProps> = {}
+export const WithFallback: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined,
+  },
+}
